ba599e9d345bbe6316358d7bcb4cc7531fafa8c3
MD Zisan <zisanislam01@gmail.com>
2023-05-18 20:34:38 +0600

    login page added

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -65,7 +65,7 @@ const handleLogin =(event)=>{
         <div className="hero min-h-screen bg-base-200">
         <div className="hero-content flex-col">
           <div className="text-center lg:text-left">
-            <h1 className="text-5xl font-bold">Login now!</h1>
+            <h1 className="text-5xl font-bold">Login now! </h1>
          
           </div> <div  className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
           <form onSubmit={handleLogin}>
