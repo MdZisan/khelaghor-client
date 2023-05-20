9815c4f9657827f7684481010345866b9ae21ce9
MD Zisan <zisanislam01@gmail.com>
2023-05-21 04:05:24 +0600

    blogs added

--- src/Components/Blogs/Blogs.jsx
+++ src/Components/Blogs/Blogs.jsx
@@ -1,15 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Blogs = () => {
-    return (
-        <div className='text-center mx-9 space-y-3 my-3'>
-           <h1>BLOG COMMING SOON</h1>
+    const [blogs,setBlogs] =useState([]);
 
-           <div className=''>
+    useEffect(()=>{
+        fetch('/blogs.json')
+        .then(res=>res.json())
+        .then(data=>setBlogs(data))
+
+
+    },[])
+
+
+
+
+
+
+    return (
+        <div className='text-center mx-9  my-3'>
+           <h1 className='font-extrabold text-3xl'>BLOG</h1>
+            <hr className='my-2'/>
+           <div className='space-y-3'>
             
-           <div className="mockup-window border bg-base-300">
-  <div className="flex justify-center px-4 py-16 bg-base-200">Hello!</div>
-</div>
+            {
+                blogs.map((blog,index)=>(
+                    <div className="mockup-window border bg-slate-400" key={index}>
+                    <div className="flex flex-col justify-center px-4 py-16 bg-slate-700 text-white">
+                        <p className='font-bold text-3xl mb-3'>{blog.question}</p>
+                        <p className='font-semibold text-xl'>{blog.answer}</p>
+                    </div>
+                  </div>
+                ))
+            }
+
+          
             </div> 
         </div>
     );
